0428a11dc770e707a0f9df4b4414d09f80a73e66
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2024-12-20 18:54:51 -0500

    Fixed the layout issue

--- PersianSky/frontend/components/Layout.tsx
+++ PersianSky/frontend/components/Layout.tsx
@@ -14,14 +14,14 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                 <Navbar.Brand href="/">
                     <span className="text-xl font-semibold">PersianSky</span>
                 </Navbar.Brand>
-                <Navbar.Items>
-                    <Navbar.Item href="/">Home</Navbar.Item>
-                    <Navbar.Item href="/listings">Listings</Navbar.Item>
-                </Navbar.Items>
+                <Navbar.Collapse>
+                    <Navbar.Link href="/">Home</Navbar.Link>
+                    <Navbar.Link href="/listings">Listings</Navbar.Link>
+                </Navbar.Collapse>
             </Navbar>
             <main className="container mx-auto p-4">{children}</main>
             <Footer>
-                <Footer.Copyright href="#" by="PersianSky" year={2023} />
+                <Footer.Copyright href="#" by="PersianSky" year={new Date().getFullYear()} />
             </Footer>
         </>
     );
